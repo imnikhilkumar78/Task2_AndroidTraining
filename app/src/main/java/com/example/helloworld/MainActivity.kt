3be3f08077e6f2fef0c507075dada68c3f813da7
unknown <Administrator@KANINI-LTP-471.gisbizdev.gisbiz.chennai.com>
2021-12-03 11:49:23 +0530

    completed

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+
+    var clickCountMap= mutableMapOf<String, Int>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val clickCountDisplayView = findViewById<TextView>(R.id.clickCountView)
+        val clickMeButton = findViewById<TextView>(R.id.clickButton)
+        val userNameTextInputView = findViewById<TextInputLayout>(R.id.nameText)
+
+        val userName = userNameTextInputView.editText?.text?.toString()
+        val maskedUserName =
+            if (userName.isNullOrEmpty()) "Somebody"
+            else userName
+
+
+
+        clickMeButton.setOnClickListener {
+
+            val userName = userNameTextInputView.editText?.text?.toString()
+            val maskedUserName =
+                if (userName.isNullOrEmpty()) "Somebody"
+                else userName
+
+            val oldClickCount=clickCountMap[maskedUserName] ?: 0
+            val newClickCount=oldClickCount+1
+
+            clickCountMap[maskedUserName]=newClickCount
+
+
+
+            clickCountDisplayView.setText("$maskedUserName Clicked $newClickCount times")
+        }
+    }
+}
